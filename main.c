8de430c1f6512ffd83b132c5ed1812f1f7ebf789
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-09-15 23:40:28 +0300

    fix(backend): properly serving files

--- main.c
+++ main.c
@@ -78,25 +78,49 @@ struct a_config a_read_args(int argc, char* argv[]) {
 
 // --- EVENTS ---
 
+typedef void (*SSRFuncPtr)(R_StringBuilder*, SSRData);
+
+char rr_uricmp(struct mg_str uri, struct mg_str exp) {
+	if (uri.len < exp.len) return 0;
+	if (strncmp(uri.buf, exp.buf, exp.len) != 0) return 0;
+	for (size_t i = exp.len; i < uri.len; i++) { // Trailing '/'
+		if (uri.buf[i] != '/') return 0;
+	}
+	return 1;
+}
+
+#define SSR_MATCH(URI_, FUNC_) \
+	if (rr_uricmp(hm->uri, mg_str(URI_))) return FUNC_;
+
+SSRFuncPtr serve_page(struct mg_connection* c, struct mg_http_message* hm) {
+	// Longer ones first
+	SSR_MATCH("/", ssr_root);
+	return NULL;
+}
+
 void ev_handle_http_msg(struct mg_connection* c, void* ev_data) {
 	struct mg_http_message* hm = (struct mg_http_message*)ev_data;
 	if (!strncmp(hm->method.buf, "GET", 3)) {
-		SSRData ssr_data = {0};
-		struct mg_str *s = mg_http_get_header(hm, "Accept-Language");
-		if (s && s->len >= 2) {
-			ssr_data.lang_is_ru = strncmp("ru", s->buf, 2) == 0;
+		SSRFuncPtr ssr_func_ptr = serve_page(c, hm);
+		printf("%.*s\n", hm->uri.len, hm->uri.buf);
+		printf("%p\n", ssr_func_ptr);
+		if (ssr_func_ptr) {
+			SSRData ssr_data = {0};
+			struct mg_str *s = mg_http_get_header(hm, "Accept-Language");
+			if (s && s->len >= 2) {
+				ssr_data.lang_is_ru = strncmp("ru", s->buf, 2) == 0;
+			}
+			//mg_http_reply(c, 200, "", "123");
+			R_StringBuilder sb = {0};
+			R_DA_RESERVE(&sb, 8192);
+			//R_SB_APPEND_CSTR(&sb, "123");
+			(*ssr_func_ptr)(&sb, ssr_data);
+			mg_http_reply(c, 200, "", sb.buf);
+			R_SB_FREE(&sb);
+			return;
 		}
-		//mg_http_reply(c, 200, "", "123");
-		R_StringBuilder sb = {0};
-		R_DA_RESERVE(&sb, 8192);
-		//R_SB_APPEND_CSTR(&sb, "123");
-		ssr_root(&sb, ssr_data);
-		mg_http_reply(c, 200, "", sb.buf);
-		R_SB_FREE(&sb);
-		return;
-		//struct mg_http_serve_opts opts = { .root_dir = aconf.web_dir, .ssi_pattern="#.shtml" };
-		//mg_http_serve_dir(c, hm, &opts);
-		//return;
+		struct mg_http_serve_opts opts = { .root_dir = aconf.web_dir };
+		mg_http_serve_dir(c, hm, &opts);
 	}
 }
 
