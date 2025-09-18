16a22eeb76fa1d4922d52177bd9163c9438ca4ef
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-09-18 22:45:41 +0300

    feat(backend): added arguments proxy for autorebuild.sh

--- autorebuild.sh
+++ autorebuild.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+ENTR_CMD="make main && ./rrwebsite \"$@\""
+
 find . -type f \
 	-name "*.c" -o \
 	-name "Makefile" -o \
 	-name "*.html" \
-	| entr -r make run
+	| entr -r bash -c "$ENTR_CMD"
