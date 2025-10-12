72fbac85b770cd2cfc09aebb94f18c4d1ec6b009
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-10-13 00:56:10 +0300

    refactor(backend): put ssr_convert functionality into nob binary

--- ssr_convert.h
+++ ssr_convert.h
@@ -1,11 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <string.h>
+#ifdef SSR_CONVERT_IMPLEMENTATION
 
-#include <dirent.h>
-
-#define NOB_IMPLEMENTATION
 #include "nob.h"
 
 char R_SB_AppendFile(Nob_String_Builder* sb, char* fpath) {
@@ -29,26 +23,26 @@ failure:
 	goto cleanup;
 }
 
-int ProcessContents(Nob_String_Builder sb_in, Nob_String_Builder* sb_out) {
-	char cb[] = "<!--%c";
-	char ce[] = "%-->";
-	char sb[] = "SSR_OUT(\"";
-	char se[] = "\");\n";
+int ProcessContents(const Nob_String_Builder sb_in, Nob_String_Builder* sb_out) {
+	char cbegin[] = "<!--%c";
+	char cend[] = "%-->";
+	char outbegin[] = "SSR_OUT(\"";
+	char outend[] = "\");\n";
 	char mode = 0;
 	size_t line = 0;
-	nob_sb_appendf(sb_out, sb);
+	nob_sb_appendf(sb_out, outbegin);
 	for (char* inp = sb_in.items; *inp; inp++) {
 		if (*inp == '\n') line++;
-		if (strncmp(inp, ce, strlen(ce)) == 0) {
-			inp = inp+strlen(ce);
+		if (strncmp(inp, cend, strlen(cend)) == 0) {
+			inp = inp+strlen(cend);
 			assert(mode != 0);
-			nob_sb_appendf(sb_out, sb);
+			nob_sb_appendf(sb_out, outbegin);
 			mode = 0;
 		}
-		if (strncmp(inp, cb, strlen(cb)) == 0) {
-			inp = inp+strlen(cb);
+		if (strncmp(inp, cbegin, strlen(cbegin)) == 0) {
+			inp = inp+strlen(cbegin);
 			assert(mode != 1);
-			nob_sb_appendf(sb_out, se);
+			nob_sb_appendf(sb_out, outend);
 			mode = 1;
 		}
 		if (mode == 0) {
@@ -57,32 +51,28 @@ int ProcessContents(Nob_String_Builder sb_in, Nob_String_Builder* sb_out) {
 		}
 		nob_da_append(sb_out, *inp);
 	}
-	nob_sb_appendf(sb_out, se);
+	nob_sb_appendf(sb_out, outend);
 	return 0;
 }
 
-int main(int argc, char* argv[]) {
-	if (argc != 4) {
-		printf("cssr_convert <in_path> <out_path> <function_name>\n");
-		printf("Example: cssr_convert something.cssr.html something_html.c\n");
-		return 0;
-	}
-
+int SSRConvert(char* in_path, char* out_path, char* func_name) {
+	printf("SSRConvert: %s -> %s : %s\n", in_path, out_path, func_name);
 	// File in
 	Nob_String_Builder sb_in = {0};
-	assert(R_SB_AppendFile(&sb_in, argv[1]));
+	assert(R_SB_AppendFile(&sb_in, in_path));
+	nob_sb_append_null(&sb_in);
 
 	// Process
 	Nob_String_Builder sb_out = {0};
-	nob_sb_appendf(&sb_out, "static inline void %s(Nob_String_Builder* ssr_sb, SSRData ssr_data);\n", argv[3]);
-	nob_sb_appendf(&sb_out, "static inline void %s(Nob_String_Builder* ssr_sb, SSRData ssr_data) {\n", argv[3]);
+	nob_sb_appendf(&sb_out, "static inline void %s(Nob_String_Builder* ssr_sb, SSRData ssr_data);\n", func_name);
+	nob_sb_appendf(&sb_out, "static inline void %s(Nob_String_Builder* ssr_sb, SSRData ssr_data) {\n", func_name);
 	assert(!ProcessContents(sb_in, &sb_out));
 	nob_sb_appendf(&sb_out, "}\n");
 
 	// File out
-	FILE *out_fp = fopen(argv[2], "w");
+	FILE *out_fp = fopen(out_path, "w");
 	if (out_fp == NULL) {
-		printf("Cannot open a dst file '%s'\n", argv[2]);
+		printf("Cannot open a dst file '%s'\n", func_name);
 		return 1;
 	}
 	size_t written = fwrite(sb_out.items, 1, sb_out.count, out_fp);
@@ -96,3 +86,5 @@ int main(int argc, char* argv[]) {
 	nob_sb_free(sb_in);
 	nob_sb_free(sb_out);
 }
+
+#endif /* SSR_CONVERT_IMPLEMENTATION */
