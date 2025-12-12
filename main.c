7d0edaebfb38e73336bfeb582e8c27c3d0560e61
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-12-12 16:48:56 +0300

    refactor(server): put ssr logic to ssr.h

--- main.c
+++ main.c
@@ -38,27 +38,9 @@ size_t active_conns = 0;
 
 // --- SSR ---
 
-typedef struct SSRData {
-	struct mg_http_message* hm;
-	char lang_is_ru;
-	size_t active_conns;
-} SSRData;
-
-#define SSR_OUT(CSTR_) nob_sb_appendf(ssr_sb, "%s", CSTR_);
-#define SSR_PRINTF(...) nob_sb_appendf(ssr_sb, __VA_ARGS__);
-
-#define SSR_PRINT_LINK(URL_) \
-	do { \
-		SSR_PRINTF("%s", URL_); \
-		SSR_PRINTF("%s", ssr_data.hm->query.len > 0 ? "?" : ""); \
-		SSR_PRINTF("%.*s", ssr_data.hm->query.len, ssr_data.hm->query.buf); \
-	} while (0);
-#define SSR_RU_PRINTF(...) \
-	if (ssr_data.lang_is_ru) { SSR_PRINTF(__VA_ARGS__); }
-#define SSR_EN_PRINTF(...) \
-	if (!ssr_data.lang_is_ru) { SSR_PRINTF(__VA_ARGS__); }
-#define SSR_DEREF_DATA(d) \
-	struct ssr_data ssr_data = (struct ssr_data*)ssr_user ? *((struct ssr_data*)ssr_user) : (struct ssr_data){0};
+#define SSR_IMPLEMENTATION
+#include "ssr.h"
+#undef SSR_IMPLEMENTATION
 
 #include "ssr_generated/ssr_template_default_before.h"
 #include "ssr_generated/ssr_template_default_after.h"
@@ -117,9 +99,6 @@ char URICompare(struct mg_str uri, struct mg_str exp) {
 	}
 	return 1;
 }
-
-#define SSR_MATCH(URI_, FUNC_) \
-	if (URICompare(hm->uri, mg_str(URI_))) return FUNC_;
 
 SSRFuncPtr HTTPServePage(struct mg_connection* c, struct mg_http_message* hm) {
 	SSR_MATCH("/", ssr_root);
